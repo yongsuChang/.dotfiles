ab764835305dad1db0191dc44a48870099094462
YongSu Chang <ahae91@gmail.com>
2025-01-28 21:55:27 +0900

    [feat] Add init file for nvimt

---
+++ nvim/init.lua
@@ -0,0 +1,47 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.g.mapleader = " "
+
+-- Font
+vim.opt.guifont = "CaskaydiaCove Nerd Font:h16"
+-- Color
+vim.opt.termguicolors = true
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
+  { import = "plugins" },
+}, {
+  -- Lazy.nvim 설정 추가
+  install = {
+    ssh = true, -- SSH 강제 사용
+  },
+  performance = lazy_config.performance,
+})
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "options"
+require "nvchad.autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
