6ca15fa13bf42ba7f7479cb4c60da9b6c682ec3f
YongSu Chang <ahae91@gmail.com>
2025-02-09 16:02:33 +0900

    [feat] Add telescope-lsp-handlers

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -12,7 +12,42 @@ local plugins = {
     "nvim-pack/nvim-spectre",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      require("spectre").setup()
+      require("spectre").setup({})
+    end,
+  },
+  -- telescope-lsp-handlers
+  {
+    "gbrlsnchs/telescope-lsp-handlers.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      local telescope = require("telescope")
+      local themes = require("telescope.themes")
+      -- Telescope의 기존 설정을 건드리지 않고 확장 옵션만 추가하는 방법입니다.
+      telescope.setup({
+        extensions = {
+          lsp_handlers = {
+            disable = {},
+            location = {
+              telescope = {},
+              no_results_message = 'No references found',
+            },
+            symbol = {
+              telescope = {},
+              no_results_message = 'No symbols found',
+            },
+            call_hierarchy = {
+              telescope = {},
+              no_results_message = 'No calls found',
+            },
+            code_action = {
+              telescope = themes.get_dropdown({}),
+              no_results_message = 'No code actions available',
+              prefix = '',
+            },
+          },
+        },
+      })
+      telescope.load_extension("lsp_handlers")
     end,
   },
   -- Colorizer 플러그인 추가
